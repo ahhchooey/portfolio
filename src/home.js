eab44b1166f81bedd37a2ed9134e7bbaa46dcff4
Alex Chui <ahhchui@gmail.com>
2019-10-15 08:52:26 -0700

    added typing effect

--- src/home.js
+++ src/home.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, {useEffect} from "react";
+import "./styles/home.css";
 
 
 function Home() {
 
+  let loopNum = 0;
+  let isDeleting = false;
+  let periodPause = 2000;
+
+  let homeText = "";
+  const thingsToSay = [
+    `Hi, I'm Alex.`,
+    `I'm a software developer.`,
+    `For me, it's all about the culture and fit.`
+  ]
+
+  useEffect(() => {
+    type();
+  }, [])
+
+  let typewriter;
+
+  const type = () => {
+    typewriter = typewriter || document.querySelector(".typewriter");
+    const i = loopNum % thingsToSay.length;
+    const text = thingsToSay[i];
+
+    if (isDeleting) {
+      homeText = text.substring(0, homeText.length - 1);
+      typewriter.innerHTML = homeText;
+    } else {
+      homeText = text.substring(0, homeText.length + 1);
+      typewriter.innerHTML = homeText;
+    }
+
+    let typeSpeed = 150 - Math.random() * 100; 
+    if (isDeleting) typeSpeed /= 2;
+
+    if (!isDeleting && homeText === text) {
+      typeSpeed = periodPause;
+      isDeleting = true;
+    } else if (isDeleting && homeText === '') {
+      isDeleting = false;
+      loopNum++;
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => {
+      type();
+      }, typeSpeed);
+  }
+
+
   return (
     <div className="home">
-      i am home
+      <div className="typewriter">
+        Hi, I'm Alex.
+      </div>
     </div>
   )
 }
