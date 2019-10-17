2adeacc1052120d571d788204ae3e0f207730fff
Alex Chui <ahhchui@gmail.com>
2019-10-16 17:16:29 -0700

    Updates

---
+++ precache-manifest.64540fa895bb2cf241ee401b4782c869.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de6442b167ff775fb0e66da6b95c1b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635ef7eba682d259447b",
+    "url": "/static/css/main.7a59bff4.chunk.css"
+  },
+  {
+    "revision": "767ef98a951c6f13e2eb",
+    "url": "/static/js/2.ff1055ee.chunk.js"
+  },
+  {
+    "revision": "635ef7eba682d259447b",
+    "url": "/static/js/main.feb4fe58.chunk.js"
+  },
+  {
+    "revision": "09d7932c83d670697ab4",
+    "url": "/static/js/runtime-main.37e4588d.js"
+  },
+  {
+    "revision": "6a6554fc505039f17a8820345abb9e47",
+    "url": "/static/media/note_screenshot.6a6554fc.png"
+  },
+  {
+    "revision": "777a63b9b56efbf61155f6be7fcc9e77",
+    "url": "/static/media/pabbot_screenshot.777a63b9.png"
+  },
+  {
+    "revision": "e56334021ff41c00a4069bda7228d6f8",
+    "url": "/static/media/safewalk_img.e5633402.png"
+  }
+]);
